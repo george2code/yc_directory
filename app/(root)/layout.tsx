f7ebd1df9fb878b3b4295dcb1ee3b2bc762d33b9
George <george2code@gmail.com>
2025-08-02 20:40:48 +0300

    add files, auth js plus github

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+ export default function Layout({children}: Readonly<{children: React.ReactNode}>) {
+    return (
+        <main className="font-work-sans">
+            <h1>Hello</h1>
+        </main>
+    )
+ }
