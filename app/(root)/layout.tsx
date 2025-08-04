bc30d2f6580789819143d3aa9242448cca422e4f
George <george2code@gmail.com>
2025-08-04 14:55:43 +0300

    auth signin signout with github provider; navbar UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,11 @@
+import Navbar from "@/app/components/Navbar";
+
  export default function Layout({children}: Readonly<{children: React.ReactNode}>) {
     return (
         <main className="font-work-sans">
-            <h1>Hello</h1>
+            <Navbar />
+
+            {children}
         </main>
     )
  }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-      HOME
+      <h1 className="text-2xl">Home</h1>
     </>
   );
 }
