bb34074ab089a466d546d1ac13c3805ff6f60350
George <george2code@gmail.com>
2025-08-05 10:36:35 +0300

    Search form; startup cards on home screen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-2xl">Home</h1>
-    </>
-  );
+export default async function Home({searchParams} : {
+  searchParams: Promise<{query?: string}>
+}) {
+    const query = (await searchParams).query;
+
+    const posts = [{ 
+        _createdAt: new Date(), 
+        views: 55, 
+        author: { _id: 1, name: 'Adrian'}, 
+        _id: 1, 
+        description: 'This is a description', 
+        image: 'https://miro.medium.com/v2/resize:fit:810/0*-1JZmBvx3DZxBR1o.jpg',
+        category: 'Robots',
+        title: 'We Robots',
+      },
+    ];
+
+    return (
+      <>
+        <section className="pink_container pattern">
+          <h1 className="font-work-sans heading">
+            Pith Your Startup, <br /> Connect With Enterpreneurs
+          </h1>
+
+          <p className='sub-heading !max-w-3xl'>
+            Submit Ideas, Vote on Pithces, and Get Noticed in Virtual Competitions
+          </p>
+
+          <SearchForm query={query} />
+        </section>
+
+
+        <section className="section_container">
+          <p className="text-30-semibold">{ query ? `Search results for "${query}"` : 'All Startups' }</p>
+
+          <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupCardType, number) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            ) : (
+              <p className='no-results'>No startups found</p>
+            )
+          }
+          </ul>
+        </section>
+      </>
+    );
 }
-
- Home
